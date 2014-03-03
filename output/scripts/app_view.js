e23734b06b8bead0a896a3ce741683b0a3957b6c
Mike Ebert <mike@mikeebert.net>
2014-03-02 19:40:43 -0600

    remove output from source

--- output/scripts/app_view.js
+++
@@ -1,9 +0,0 @@
-var AppView = Backbone.View.extend({
-  el: $('#gallery-container'),
-  render: function() {
-    _.each( new Thumbs().fetch(), function(thumb) {
-        $('div ul#thumbnails').append( new ThumbView({ model: thumb }).render().el)
-      });
-    $('#thumbnails').children()[0].click();
-  }
-});
